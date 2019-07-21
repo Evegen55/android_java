46fc6a5d1d2c1f0119db9b16c53a404830f58de5
Evgenii Lartcev <i.dolende@gmail.com>
2019-07-21 14:59:00 +0300

    getting list of sensors

--- app/src/main/java/com/example/first_android_app/MainActivity.java
+++ app/src/main/java/com/example/first_android_app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.first_android_app;
 
 import android.content.Context;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,8 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,16 +31,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-
-
-                    SensorManager systemService = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-
-
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("TODO");
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(getSensorsNames());
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -38,13 +44,21 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    private StringBuilder getSensorsNames() {
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        final StringBuilder sb = new StringBuilder();
+        for (Sensor sensor : deviceSensors) {
+            sb.append(sensor.getName())
+                    .append("\n")
+//                    .append("vendor")
+//                    .append("\t")
+//                    .append(sensor.getVendor())
+//                    .append("\n\n")
+            ;
+        }
+        return sb;
     }
 
 }
